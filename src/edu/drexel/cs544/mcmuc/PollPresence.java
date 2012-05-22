6d8f1f998397fc5a4f131727f9e0fd7f358600b2
Brian Balderston <balderstonb@gmail.com>
2012-05-22 07:41:37 -0400

    javadoc commented

--- src/edu/drexel/cs544/mcmuc/PollPresence.java
+++ src/edu/drexel/cs544/mcmuc/PollPresence.java
@@ -3,13 +3,34 @@ package edu.drexel.cs544.mcmuc;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * PollPresence is used to query the other clients in a chat room about their online/offline status.
+ * Clients in the room should respond with a broadcast Presence message.
+ * 
+ * The JSON format of a PollPresence is {'uid':'<uid>','action':'poll-presence'}
+ */
 public class PollPresence extends Action implements JSON {
 	
+	/**
+	 * No options are required for the construction of a PollPresence message
+	 */
+	public PollPresence()
+	{
+	}
+	
+	/**
+	 * Deserializes the JSON into a PollPresence object
+	 * is accepted.
+	 * @param json
+	 */
 	public PollPresence(JSONObject json)
 	{
 		super(json,"poll-presence");
 	}
 	
+	/**
+	 * Serializes the PollPresence object into JSON
+	 */
 	@Override
 	public JSONObject toJSON() {
 		JSONObject json = new JSONObject();
