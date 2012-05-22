31bd21b6f7fdfa163242f38412016417411c01a6
Brian Balderston <balderstonb@gmail.com>
2012-05-21 21:42:38 -0400

    rooms are optional

--- src/edu/drexel/cs544/mcmuc/ListRooms.java
+++ src/edu/drexel/cs544/mcmuc/ListRooms.java
@@ -9,60 +9,73 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ListRooms extends Action implements JSON {
-	
-	public List<Integer> rooms = new ArrayList<Integer>();
-	
+
+	public List<Integer> rooms;
+
 	public ListRooms(List<Integer> rooms){
 		this.rooms = rooms;
 	}
-		
+
+	public ListRooms()
+	{
+	}
+
 	public List<Integer> getRooms()	{
 		return this.rooms;
 	}
-	
+
 	public void addRoom(Integer room){
 		this.rooms.add(room);
 	}
-	
+
 	public ListRooms(JSONObject json)
 	{
 		super(json,"list-rooms");
-		
+
 		try {
-			JSONArray rooms = json.getJSONArray("rooms");
-			
-			if (rooms != null) { 
-				   int len = rooms.length();
-				   for (int i=0;i<len;i++){ 
-					   this.rooms.add((Integer) rooms.get(i));
-				   } 
-			} 
+			if(json.has("rooms"))
+			{
+				JSONArray rooms = json.getJSONArray("rooms");
+
+				if (rooms != null)
+				{ 
+					int len = rooms.length();
+					this.rooms = new ArrayList<Integer>(len);
+					for (int i=0;i<len;i++)
+					{ 
+						this.rooms.add((Integer) rooms.get(i));
+					} 
+				} 
+			}
 
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}		
 	}
-	
+
 	@Override
 	public JSONObject toJSON() {
 		JSONObject json = new JSONObject();	
 		JSONArray list = new JSONArray();
-		Iterator<Integer> itr = rooms.iterator();
-		
+
 		try {
-			
-			while(itr.hasNext()){
-				list.put(itr.next());
+			if(rooms != null)
+			{
+				Iterator<Integer> itr = rooms.iterator();
+				while(itr.hasNext())
+				{
+					list.put(itr.next());
+				}
+				json.put("rooms", list);
 			}
-			
 			json.put("action", "list-rooms");
 			json.put("uid", uid);
-			json.put("rooms", list);
+
 		} catch (JSONException e) {
 
 		}
-		
+
 		return json;
 	}
-	
+
 }
