221135a17f878d7980e09e06769de50df979e678
Brian Balderston <balderstonb@gmail.com>
2012-05-22 06:27:25 -0400

    javadoc comments

--- src/edu/drexel/cs544/mcmuc/JSON.java
+++ src/edu/drexel/cs544/mcmuc/JSON.java
@@ -1,7 +1,16 @@
 package edu.drexel.cs544.mcmuc;
 
 import org.json.JSONObject;
-
+/**
+ * JSON is an interface implemented by each message type that provides a standard way 
+ * to retrieve the JSON representation of a class.
+ */
 public interface JSON {
+	/**
+	 * Serializes the class in JSON notation
+	 * @return the JSONObject that carries the required and optional fields that define 
+	 * a message type
+	 * @see JSONObject
+	 */
 	public JSONObject toJSON();
 }
