487c418fd20001b0a52af85c72bed63cd6641678
Brian Balderston <balderstonb@gmail.com>
2012-05-23 20:49:37 -0400

    comments

--- src/edu/drexel/cs544/mcmuc/actions/ActionBase.java
+++ src/edu/drexel/cs544/mcmuc/actions/ActionBase.java
@@ -6,7 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Action is the base class for all message types. All messages must have an unique identifier,
+ * ActionBase is the base class for all message types. All messages must have an unique identifier,
  * which is automatically assigned for new Actions, and restored for Actions created through JSON
  * deserialization. If an Action is created by deserializing JSON, an expected action type that
  * is compared against the found type must be passed in.
@@ -16,7 +16,7 @@ public class ActionBase {
     String uid;
 
     /**
-     * The default constructor for Action only assigns a random UUID as the message's unique
+     * The default constructor for ActionBase only assigns a random UUID as the message's unique
      * identifier
      */
     public ActionBase() {
@@ -43,15 +43,23 @@ public class ActionBase {
         }
     }
 
+    /**
+     * Does not check against the action type and does not perform any
+     * additional deserialization - this is left to the child classes to implement.
+     * @param json the serialized JSON representation of the Action
+     */
     public ActionBase(JSONObject json) {
         try {
             uid = json.getString("uid");
         } catch (JSONException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
+    /**
+     * Accessor for UID
+     * @return String uid
+     */
     public String getUID() {
         return uid;
     }
