a02c0ed7b947e51e21b7b76ae7394b86f7f0d681
Brian Balderston <balderstonb@gmail.com>
2012-05-25 21:05:14 -0400

    documented CLLQ

--- src/edu/drexel/cs544/mcmuc/ConcurrentLimitedLinkedQueue.java
+++ src/edu/drexel/cs544/mcmuc/ConcurrentLimitedLinkedQueue.java
@@ -3,9 +3,12 @@ package edu.drexel.cs544.mcmuc;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * Extends a CLQ to have a maximum number of elements
+ * ConcurrentLinkedQueue is thread-safe queue that orders elements FIFO. New elements are
+ * inserted at the tail of the queue, and the queue retrieval operations obtain elements 
+ * at the head of the queue. A ConcurrentLinkedQueue allows mulitple threads to share access
+ * to a the collection.
  * 
- * @author di
+ * ConcurrentLimitedLinkedQueue extends a CLQ to have a maximum number of elements
  */
 @SuppressWarnings("serial")
 public class ConcurrentLimitedLinkedQueue<E> extends ConcurrentLinkedQueue<E> {
@@ -13,10 +16,20 @@ public class ConcurrentLimitedLinkedQueue<E> extends ConcurrentLinkedQueue<E> {
     // The limit size of the queue
     private int limit;
 
+    /**
+     * Creates a CLQ whose size cannot exceed limit - any additions will remove the 
+     * element at the head of the queue and add the element to the tail.
+     * @param limit int maximum size
+     */
     public ConcurrentLimitedLinkedQueue(int limit) {
         this.limit = limit;
     }
 
+    /**
+     * if the size of the CLQ has reached limit, the addition of 'o' will remove the 
+     * element at the head of the queue and add 'o' to the tail. If 'o' was added, add
+     * returns true, and false otherwise.
+     */
     @Override
     public boolean add(E o) {
         // Remove if the queue size is above the limit
