a3b270bb378dc9fdebc4410e4c9095efba7537f5
Dustin Ingram <dustin.ingram@gmail.com>
2012-05-29 18:03:32 -0400

    Formatting.

--- src/edu/drexel/cs544/mcmuc/Controller.java
+++ src/edu/drexel/cs544/mcmuc/Controller.java
@@ -17,7 +17,7 @@ import edu.drexel.cs544.mcmuc.actions.Timeout;
 import edu.drexel.cs544.mcmuc.actions.UseRooms;
 
 /**
- * Controller represents a control channel, which is a fixed port for the sending of 
+ * Controller represents a control channel, which is a fixed port for the sending of
  * messages, such as room creation and detection, that must reach all clients on the network.
  * Controller exposes two sets of ports - all ports in use, and the ports in use that for which
  * the client is actively using the room.
@@ -33,6 +33,7 @@ public class Controller extends Channel {
 
     /**
      * Creates the controller channel on the given port and adds it to the portsInUse set
+     * 
      * @param port int port to use (will always be CONTROL_PORT)
      */
     private Controller(int port) {
@@ -45,6 +46,7 @@ public class Controller extends Channel {
     /**
      * Restricts the instantiation of the Controller class to one object (the Singleton
      * design pattern)
+     * 
      * @return Controller singleton
      */
     public static Controller getInstance() {
@@ -70,59 +72,61 @@ public class Controller extends Channel {
         if (actionString.equalsIgnoreCase(UseRooms.action)) {
             action = new UseRooms(jo);
             action.process(this);
-        } else if(actionString.equalsIgnoreCase(ListRooms.action)) {
-        	action = new ListRooms(jo);
-        	action.process(this);
-        } else if(actionString.equalsIgnoreCase(Timeout.action)) {
-        	action = new Timeout(jo);
-        	action.process(this);
-        } else if(actionString.equalsIgnoreCase(Preserve.action)) {
-        	action = new Preserve(jo);
-        	action.process(this);
+        } else if (actionString.equalsIgnoreCase(ListRooms.action)) {
+            action = new ListRooms(jo);
+            action.process(this);
+        } else if (actionString.equalsIgnoreCase(Timeout.action)) {
+            action = new Timeout(jo);
+            action.process(this);
+        } else if (actionString.equalsIgnoreCase(Preserve.action)) {
+            action = new Preserve(jo);
+            action.process(this);
         } else {
-        
+
             System.err.println("Message action type not supported: " + actionString);
         }
     }
 
     /**
      * Takes the given displayString and sends it to System.out
+     * 
      * @param displayString String to display
      */
     public void display(String displayString) {
         System.out.println(displayString);
     }
-    
+
     /**
      * Resets the primary timer for the Room on roomPort
+     * 
      * @param roomPort int port of room to reset primary timer for
      */
-    public void resetPrimaryTimer(int roomPort)
-    {
-    	rooms.get(roomPort).resetPrimaryTimer();
+    public void resetPrimaryTimer(int roomPort) {
+        rooms.get(roomPort).resetPrimaryTimer();
     }
-    
+
     /**
      * Stops the primary timer for the Room on roomPort
+     * 
      * @param roomPort int port of room to stop primary timer for
      */
-    public void stopPrimaryTimer(int roomPort)
-    {
-    	rooms.get(roomPort).stopPrimaryTimer();
+    public void stopPrimaryTimer(int roomPort) {
+        rooms.get(roomPort).stopPrimaryTimer();
     }
-    
+
     /**
      * Starts the secondary timer for the Room on roomPort
+     * 
      * @param roomPort int port of room to start secondary timer for
      */
-    public void startSecondaryTimer(int roomPort)
-    {
-    	rooms.get(roomPort).startSecondaryTimer();
+    public void startSecondaryTimer(int roomPort) {
+        rooms.get(roomPort).startSecondaryTimer();
     }
 
     /**
      * If the set portsInUse does not contain roomPort, create a new Forwarder on that port,
-     * and add the port to portsInUse. 
+     * and add the port to portsInUse.
+     * 
      * @param roomPort int
      */
     public void useRoom(int roomPort) {
@@ -134,48 +138,47 @@ public class Controller extends Channel {
             // This port is already in use, either by a Room or Forwarder
         }
     }
-    
+
     /**
      * If the set portsInUse contains roomPort, remove it. If there is a forwarder on the
      * port, remove it. Otherwise, if there is a room on the port, remove it and remove the
      * port from roomPortsInUse. If there was a room, set the client's presence to Offline.
+     * 
      * @param roomPort int port to leave
      */
-    public void leaveRoom(int roomPort){
-    	if(portsInUse.contains(roomPort))
-    	{
-    		portsInUse.remove(roomPort);
-    		if(forwards.containsKey(roomPort))
-    			forwards.remove(roomPort);
-    		else if(roomPortsInUse.contains(roomPort))
-    		{
-    			roomPortsInUse.remove(roomPort);
-    			if(rooms.containsKey(roomPort))
-				{
-    				Room r = rooms.remove(roomPort);
-    				r.setStatus(Status.Offline);
-				}
-    		}
-    	}
+    public void leaveRoom(int roomPort) {
+        if (portsInUse.contains(roomPort)) {
+            portsInUse.remove(roomPort);
+            if (forwards.containsKey(roomPort))
+                forwards.remove(roomPort);
+            else if (roomPortsInUse.contains(roomPort)) {
+                roomPortsInUse.remove(roomPort);
+                if (rooms.containsKey(roomPort)) {
+                    Room r = rooms.remove(roomPort);
+                    r.setStatus(Status.Offline);
+                }
+            }
+        }
     }
-    
+
     /**
      * Remove the room that corresponds to the name given by roomName, set that client's status
      * to Offline, and remove the port from rooomPortsInUse.
+     * 
      * @param roomName String name of room to leave
      */
-    public void leaveRoom(String roomName)
-    {
-    	Integer p = roomNames.remove(roomName);
-    	Room r = rooms.remove(p);
-    	r.setStatus(Status.Offline);
-    	roomPortsInUse.remove(r.getPort());
+    public void leaveRoom(String roomName) {
+        Integer p = roomNames.remove(roomName);
+        Room r = rooms.remove(p);
+        r.setStatus(Status.Offline);
+        roomPortsInUse.remove(r.getPort());
     }
 
     /**
      * Create a new room on the port chosen by the hash algorithm given the roomName and portsInUse.
      * Set the user's name in the room to userName. Add the room and add the port to both
      * portsInUse and roomPortsInUse.
+     * 
      * @param roomName String name of room to use
      * @param userName String name to associate with user in the room
      */
@@ -185,17 +188,17 @@ public class Controller extends Channel {
         roomPortsInUse.add(room.getPort());
         rooms.put(room.getPort(), room);
         roomNames.put(roomName, room.getPort());
-        
+
     }
-    
+
     /**
      * Set that user's presence in the room associated with roomName to the given presence
+     * 
      * @param roomName String name of room to set presence for
      * @param presence Presence to set
      * @see Status
      */
-    public void setRoomStatus(String roomName, Status presence)
-    {
+    public void setRoomStatus(String roomName, Status presence) {
         Room room = rooms.get(roomNames.get(roomName));
         if (room != null) {
             room.setStatus(presence);
@@ -206,11 +209,12 @@ public class Controller extends Channel {
 
     /**
      * Send the given action to the room associated with roomName
+     * 
      * @param roomName String name of room to send action to
      * @param action Action to send
      */
     public void sendToRoom(String roomName, Action action) {
-    	Room room = rooms.get(roomNames.get(roomName));
+        Room room = rooms.get(roomNames.get(roomName));
         if (room != null) {
             room.send(action);
         } else {
