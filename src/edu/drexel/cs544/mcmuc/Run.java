dbe9b346f8de18442862cca87794cfb4475572a3
Brian Balderston <balderstonb@gmail.com>
2012-06-02 16:59:12 -0400

    documented Run

--- src/edu/drexel/cs544/mcmuc/Run.java
+++ src/edu/drexel/cs544/mcmuc/Run.java
@@ -3,9 +3,31 @@ package edu.drexel.cs544.mcmuc;
 import edu.drexel.cs544.mcmuc.actions.Message;
 import edu.drexel.cs544.mcmuc.actions.Presence.Status;
 
+/**
+ * Run exists to exercise a simple command-line interface to the Multicast Multi-User Chat protocol.
+ * 
+ * The Multicast Multi-User Chat is a distributed, non-reliable application-layer protocol that uses UDP
+ * at the transport layer and provides multi-user chat room creation, discovery, and the exchange of
+ * user presence information and messaging without a central server. A client can join a chat room
+ * by name, which is hashed to a specific multicast channel within a given range of channels, excluding
+ * those already in use. The client then sends messages intended for that multi-user chat on the 
+ * channel produced by the hashing algorithm. The protocol is distributed and decentralized by using a 
+ * classical flooding algorithm to forward non-duplicate messages.
+ * 
+ * @author Brian Balderston
+ * @author Dustin Ingram
+ * @author Clint Kirberger
+ * @author Ben Schilke
+ */
 public class Run {
     public static final int CONTROL_PORT = 31941;
 
+    /**
+     * Create and start a new command-line interface. Create a new Controller and set the user interface
+     * to be the previously created command-line interface. Continuously receive and process commands from
+     * the user until the received command is 'exit'.
+     * @param args String command-line arguments to program, which are ignored
+     */
     public static void main(String[] args) {
 
         CLI cli = new CLI();
@@ -13,11 +35,6 @@ public class Run {
 
         Controller controller = Controller.getInstance();
         controller.setUI(cli);
-
-        controller.useRoom("testchannel", "dustin");
-        // controller.useRoom(52316);
-        controller.sendToRoom("testchannel", new Message("dustin", "Hello World"));
-        controller.setRoomStatus("testchannel", Status.Offline);
 
         while (true) {
             CLICommand command = cli.getNextCommand();
